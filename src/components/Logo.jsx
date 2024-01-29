6f65504de8a2cd8b83bd16270a9ba7d0cf9cff37
Mohit Soni <sonimohit481@gmail.com>
2024-01-29 19:17:15 +0530

    Dummy Logo added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Logo = ({ width = "100px" }) => {
-  return <div>Logo</div>;
+  return (
+    <div>
+      {" "}
+      <img
+        src="https://png.pngtree.com/png-vector/20230304/ourmid/pngtree-colorful-blog-speech-bubble-vector-png-image_6633021.png"
+        alt="Logo"
+        width={width}
+      />
+    </div>
+  );
 };
 
 export default Logo;
