09aa07c796ff02b4da10f2403e50297960ce38c9
Mohit Soni <sonimohit481@gmail.com>
2024-01-29 19:31:50 +0530

    Components Created

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,27 @@ import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import RTE from "./RTE";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Login from "./Login";
+import Signup from "./Signup";
 
-export { Container, Logo, LogoutBtn, Header, Footer };
+export {
+  Container,
+  Logo,
+  LogoutBtn,
+  Header,
+  Footer,
+  Button,
+  Input,
+  Select,
+  RTE,
+  PostCard,
+  AuthLayout,
+  Login,
+  Signup,
+};
