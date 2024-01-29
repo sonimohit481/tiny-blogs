bbaad8769d4e791290be950a00ad95e52df65edd
Mohit Soni <sonimohit481@gmail.com>
2024-01-29 19:04:20 +0530

    Header Footer Created

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+
+export { Container, Logo, LogoutBtn, Header, Footer };
