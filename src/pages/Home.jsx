5c498b892ab1da6560dc69da3b66e38269779b30
Mohit Soni <sonimohit481@gmail.com>
2024-01-29 19:43:42 +0530

    All pages created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import appwriteService from "../appwrite/config";
-import { Container } from "../components";
+import { Container, PostCard } from "../components";
 
 function Home() {
   const [posts, setPosts] = useState([]);
@@ -34,7 +34,7 @@ function Home() {
         <div className="flex flex-wrap">
           {posts.map((post) => (
             <div key={post.$id} className="p-2 w-1/4">
-              {/* <PostCard {...post} /> */}
+              <PostCard {...post} />
             </div>
           ))}
         </div>
