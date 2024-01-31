9339f6d7ca775cdf0d6058f2b78b78c7138008de
Mohit Soni <sonimohit481@gmail.com>
2024-01-31 18:18:34 +0530

    Basic structure done

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, PostCard } from "../components";
 import appwriteService from "../appwrite/config";
 
-function AllPosts() {
+const AllPosts = () => {
   const [posts, setPosts] = useState([]);
   useEffect(() => {}, []);
   appwriteService.getPosts([]).then((posts) => {
@@ -23,6 +23,6 @@ function AllPosts() {
       </Container>
     </div>
   );
-}
+};
 
 export default AllPosts;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import Home from "./Home";
-
-export { Home };
+import AllPosts from "./AllPosts";
+import EditPost from "./EditPost";
+import AddPost from "./AddPost";
+import Login from "./Login";
+import Post from "./Post";
+import Signup from "./Signup";
+export { Home, AllPosts, EditPost, AddPost, Login, Post, Signup };
